f7a2dd91815daff529c5e838c8a5794d79f13a9a
Auston Stewart <auston@shedbuilt.net>
2017-12-17 17:22:50 -1000

    Adding a patch to fix compilation against gperf 3.1

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# Suppress static library
 sed -i '/install.*STALIBNAME/d' libcap/Makefile
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} RAISE_SETFCAP=no lib=lib prefix=/usr install
-chmod -v 755 ${SHED_FAKEROOT}/usr/lib/libcap.so
-mkdir -v ${SHED_FAKEROOT}/lib
-mv -v ${SHED_FAKEROOT}/usr/lib/libcap.so.* ${SHED_FAKEROOT}/lib
-ln -sfv ../../lib/$(readlink ${SHED_FAKEROOT}/usr/lib/libcap.so) ${SHED_FAKEROOT}/usr/lib/libcap.so
+# Address compilation issue when gperf-3.1+ is installed
+patch -Np1 -i "$SHED_PATCHDIR/libcap-2.25-with-gperf-3.1.patch"
+make -j $SHED_NUMJOBS || exit 1
+make "DESTDIR=${SHED_FAKEROOT}" RAISE_SETFCAP=no lib=lib prefix=/usr install || exit 1
+chmod -v 755 "${SHED_FAKEROOT}/usr/lib/libcap.so"
+mkdir -v "${SHED_FAKEROOT}/lib"
+mv -v ${SHED_FAKEROOT}/usr/lib/libcap.so.* "${SHED_FAKEROOT}/lib"
+ln -sfv "../../lib/$(readlink ${SHED_FAKEROOT}/usr/lib/libcap.so)" "${SHED_FAKEROOT}/usr/lib/libcap.so"
