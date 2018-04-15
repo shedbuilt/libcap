3e374dd8ccc494676d7d6b488c83d40a56811fa0
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:25:07 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # Suppress static library
 sed -i '/install.*STALIBNAME/d' libcap/Makefile
 # Address compilation issue when gperf-3.1+ is installed
-patch -Np1 -i "$SHED_PATCHDIR/libcap-2.25-with-gperf-3.1.patch"
-make -j $SHED_NUMJOBS || exit 1
-make "DESTDIR=${SHED_FAKEROOT}" RAISE_SETFCAP=no lib=lib prefix=/usr install || exit 1
-chmod -v 755 "${SHED_FAKEROOT}/usr/lib/libcap.so"
-mkdir -v "${SHED_FAKEROOT}/lib"
-mv -v ${SHED_FAKEROOT}/usr/lib/libcap.so.* "${SHED_FAKEROOT}/lib"
-ln -sfv "../../lib/$(readlink ${SHED_FAKEROOT}/usr/lib/libcap.so)" "${SHED_FAKEROOT}/usr/lib/libcap.so"
+patch -Np1 -i "$SHED_PKG_PATCH_DIR/libcap-2.25-with-gperf-3.1.patch"
+make -j $SHED_NUM_JOBS || exit 1
+make "DESTDIR=${SHED_FAKE_ROOT}" RAISE_SETFCAP=no lib=lib prefix=/usr install || exit 1
+chmod -v 755 "${SHED_FAKE_ROOT}/usr/lib/libcap.so"
+mkdir -v "${SHED_FAKE_ROOT}/lib"
+mv -v ${SHED_FAKE_ROOT}/usr/lib/libcap.so.* "${SHED_FAKE_ROOT}/lib"
+ln -sfv "../../lib/$(readlink ${SHED_FAKE_ROOT}/usr/lib/libcap.so)" "${SHED_FAKE_ROOT}/usr/lib/libcap.so"
